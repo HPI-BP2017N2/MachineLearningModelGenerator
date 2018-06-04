6eec39c8c39ec6c89cb25f15811e3dfe112fc0dd
Daniel Lindner <lindnerdaniel@yahoo.de>
2018-06-04 14:27:27 +0200

    add swagger API documentation

--- src/main/java/de/hpi/modelgenerator/api/ModelGeneratorController.java
+++ src/main/java/de/hpi/modelgenerator/api/ModelGeneratorController.java
@@ -2,6 +2,9 @@ package de.hpi.modelgenerator.api;
 
 import de.hpi.modelgenerator.persistence.ClassifierTrainingState;
 import de.hpi.modelgenerator.services.ModelGeneratorService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -23,23 +26,39 @@ public class ModelGeneratorController {
     private final ClassifierTrainingState brandClassifierTrainingState = new ClassifierTrainingState();
     private final ClassifierTrainingState modelTrainingState = new ClassifierTrainingState();
 
+    @ApiOperation(value = "Generate category classifier")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully generated category classifier."),
+            @ApiResponse(code = 500, message = "There was an error (serializing the classifier).")})
     @RequestMapping(value = "/generateCategoryClassifier", method = RequestMethod.POST)
     public void generateCategoryClassifier() throws IOException {
         getService().generateCategoryClassifier(getCategoryClassifierTrainingState());
 
     }
 
+    @ApiOperation(value = "Generate brand classifier")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully generated brand classifier."),
+            @ApiResponse(code = 500, message = "There was an error (serializing the classifier).")})
     @RequestMapping(value = "/generateBrandClassifier", method = RequestMethod.POST)
     public void generateBrandClassifier() throws IOException {
         getService().generateBrandClassifier(getBrandClassifierTrainingState());
 
     }
 
+    @ApiOperation(value = "Generate model")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully generated model."),
+            @ApiResponse(code = 500, message = "There was an error (serializing the model). Make sure that brand classifier is present.")})
     @RequestMapping(value = "/generateModel", method = RequestMethod.POST)
     public void generateModel() throws IOException {
         getService().generateModel(getModelTrainingState());
     }
 
+    @ApiOperation(value = "Generate all classifiers")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully generated all classifier."),
+            @ApiResponse(code = 500, message = "There was an error (serializing the classifiers).")})
     @RequestMapping(value = "/generateAllClassifiers", method = RequestMethod.POST)
     public void generateAllClassifiers() throws IOException {
         getService().refreshTrainingAndTestingSet();
